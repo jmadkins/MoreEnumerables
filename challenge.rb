83b09544cd4db2febeaa0101586cb6814266f1b4
Justin Adkins <jadkins@covermymeds.com>
2018-05-31 12:48:23 -0400

    Finished and got all trues!

--- challenge.rb
+++ challenge.rb
@@ -1,40 +1,40 @@
 
 def capitalize_each_string(input)
-  #implement your solution here
+  input.map(&:capitalize)
 end
 
 def fetch_the_dog(input)
-  #implement your solution here
+  input.select { |i| i == 'dog' }
 end
 
 def no_dogs_allowed(input)
-  #implement your solution here
+  input.reject { |i| i == 'dog' }
 end
 
 def count_the_animals(input)
-  #implement your solution here
+  input.length
 end
 
 def fetch_the_first_two(input)
-  #implement your solution here
+  input.take(2)
 end
 
 def fetch_CD_animals(input)
-  #implement your solution here
+  input.find_all { |i| i == 'cat' || i == 'dog' }
 end
 
 ## DO NOT CHANGE CODE BELOW THIS LINE ##
 
-animals = ["cat", "moose", "dog", "bird"]
+animals = %w[cat moose dog bird]
 
-p capitalize_each_string(animals) == ["Cat", "Moose", "Dog", "Bird"]
+p capitalize_each_string(animals) == %w[Cat Moose Dog Bird]
 
-p fetch_the_dog(animals) == ["dog"] 
+p fetch_the_dog(animals) == ['dog']
 
-p no_dogs_allowed(animals) == ["cat", "moose", "bird"]
+p no_dogs_allowed(animals) == %w[cat moose bird]
 
 p count_the_animals(animals) == 4
 
-p fetch_the_first_two(animals)  == ["cat", "moose"]
+p fetch_the_first_two(animals) == %w[cat moose]
 
-p fetch_CD_animals(animals) == ["cat", "dog"]
+p fetch_CD_animals(animals) == %w[cat dog]
